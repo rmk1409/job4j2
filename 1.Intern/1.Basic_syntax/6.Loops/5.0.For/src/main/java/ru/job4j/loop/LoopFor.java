0bf29bedc77b71372eaec1985a414a03f5d28a7d
Alex Veselov <rmk140987@gmail.com>
2020-03-15 12:16:55 +0300

    5.0. For loop [#174325]

---
+++ 1.Intern/1.Basic_syntax/6.Loops/5.0.For/src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+/**
+ * Доработайте класс ru.job4j.loop.LoopFor. Нужно вывести на консоль числа от 5 до 10
+ */
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int index = 5; index <= 10; index++) {
+            System.out.println(index);
+        }
+    }
+}
