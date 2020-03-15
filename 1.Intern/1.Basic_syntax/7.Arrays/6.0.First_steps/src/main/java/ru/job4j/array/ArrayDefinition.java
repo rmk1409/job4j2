dcb1992c4fcebb3fb08483598776a62c1450e137
Alex Veselov <rmk140987@gmail.com>
2020-03-15 20:22:15 +0300

    6. Массивы. [#174341]

---
+++ 1.Intern/1.Basic_syntax/7.Arrays/6.0.First_steps/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * . Внутри метода main создайте три переменных.
+ * - Массив на 10 элементов типа short с именем ages.
+ * - Массив на 100500 элементов типа String c именем surnames.
+ * - Массив на 40 элементов типа float с именем prices.
+ * Created by Alex on 15.03.2020
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
