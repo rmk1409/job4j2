273faaf9ff6015d33224048eec8aaf9227d396a1
Alex Veselov <rmk140987@gmail.com>
2020-03-17 13:42:55 +0300

    1.1.Exam, Basic Syntax, Объединить два массива [#124482]

---
+++ 1.Intern/1.Basic_syntax/8.Exam/src/main/java/ru/job4j/ArrayMerger.java
@@ -0,0 +1,33 @@
+package ru.job4j;
+
+import java.util.Arrays;
+
+/**
+ * Combine two sorted arrays.
+ * Created by Roman Pogorelov on 17.03.2020
+ */
+public class ArrayMerger {
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            rsl[k++] = left[i] < right[j] ? left[i++] : right[j++];
+        }
+        while (i < left.length) {
+            rsl[k++] = left[i++];
+        }
+        while (j < right.length) {
+            rsl[k++] = right[j++];
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        ArrayMerger process = new ArrayMerger();
+        int[] rsl = process.merge(
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
---
+++ 1.Intern/1.Basic_syntax/8.Exam/src/test/java/ru/job4j/ArrayMergerTest.java
@@ -0,0 +1,66 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by Roman Pogorelov on 17.03.2020
+ */
+public class ArrayMergerTest {
+    @Test
+    public void whenBothEmpty() {
+        ArrayMerger algo = new ArrayMerger();
+        int[] expect = new int[0];
+        int[] result = algo.merge(
+                new int[0],
+                new int[0]
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAscOrder() {
+        ArrayMerger algo = new ArrayMerger();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[]{1, 2},
+                new int[]{3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenLeftLess() {
+        ArrayMerger algo = new ArrayMerger();
+        int[] expect = {1, 2, 3, 3, 4};
+        int[] result = algo.merge(
+                new int[]{1, 2, 3},
+                new int[]{3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenLeftGreat() {
+        ArrayMerger algo = new ArrayMerger();
+        int[] expect = {1, 2, 3, 4, 4};
+        int[] result = algo.merge(
+                new int[]{1, 2},
+                new int[]{3, 4, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenLeftEmpty() {
+        ArrayMerger algo = new ArrayMerger();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[]{},
+                new int[]{1, 2, 3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+}
