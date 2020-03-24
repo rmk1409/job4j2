9cafc6c6199c71921c4ebf187a009e08a840653d
Alex Veselov <rmk140987@gmail.com>
2020-03-24 11:22:15 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#174434]

---
+++ 1.Intern/2.OOP/src/main/java/ru/job4j/tracker/StubAction.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker;
+
+import ru.job4j.tracker.action.UserAction;
+
+/**
+ * Created by Roman Pogorelov on 24.03.2020
+ */
+public class StubAction implements UserAction {
+    private boolean call = false;
+
+    @Override
+    public String name() {
+        return "Stub action";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        call = true;
+        return false;
+    }
+
+    public boolean isCall() {
+        return call;
+    }
+}
--- 1.Intern/2.OOP/src/test/java/ru/job4j/tracker/StartUITest.java
+++ 1.Intern/2.OOP/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,49 +1,22 @@
-//package ru.job4j.tracker;
-//
-//import org.junit.Test;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.is;
-//import static org.hamcrest.Matchers.nullValue;
-//
-///**
-// * Created by Roman Pogorelov on 20.03.2020
-// */
-//public class StartUITest {
-//    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
-//    }
-//
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answers = {
-//                item.getId(), // id сохраненной заявки в объект tracker.
-//                "replaced item"
-//        };
-//        StartUI.editItem(new StubInput(answers), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("replaced item"));
-//    }
-//
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answers = {
-//                item.getId()
-//        };
-//        StartUI.deleteItem(new StubInput(answers), tracker);
-//        assertThat(tracker.findById(item.getId()), is(nullValue()));
-//    }
-//}
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.tracker.action.UserAction;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by Roman Pogorelov on 20.03.2020
+ */
+public class StartUITest {
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[]{"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[]{action});
+        assertThat(action.isCall(), is(true));
+    }
+}
